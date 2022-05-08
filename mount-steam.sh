e73f82ad7ce4b1c6a1b5abe7243fa7e05788c32a
eusouobn <86791806+eusouobn@users.noreply.github.com>
2022-05-07 21:48:26 -0300

    Add files via upload

---
+++ mount-steam.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+sudo mkdir /steam
+sudo mkdir /lutris
+sudo chown -R bn:bn /steam
+sudo chown -R bn:bn /lutris
+sudo chmod 777 /steam
+sudo chmod 777 /lutris
+
+sudo blkid -s UUID -o value /dev/sdb1 | while read line
+do
+echo -e "\nUUID=$line /steam  btrfs  defaults  0 0" | sudo tee -a /etc/fstab
+done
+
+sudo blkid -s UUID -o value /dev/sdb1 | while read line
+do
+echo -e "\nUUID=$line /lutris  btrfs  defaults  0 0" | sudo tee -a /etc/fstab
+done
