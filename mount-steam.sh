9d184616e6864bdf258de1dd1f76969cbce65223
eusouobn <86791806+eusouobn@users.noreply.github.com>
2022-05-07 21:14:40 -0300

    Update mount-steam.sh

--- mount-steam.sh
+++ mount-steam.sh
@@ -6,11 +6,15 @@ sudo chown -R bn:bn /steam
 sudo chown -R bn:bn /lutris
 sudo chmod 777 /steam
 sudo chmod 777 /lutris
-sudo blkid -s UUID -o value /dev/sdb1 | while read line
-do
-echo -e "\nUUID=$line /steam  btrfs  defaults  0 0" | sudo tee -a /etc/fstab
 
 sudo blkid -s UUID -o value /dev/sdb1 | while read line
+if [  $(grep -c steam /etc/fstab) != 0 ]; then
+do
+echo -e "\nUUID=$line /steam  btrfs  defaults  0 0" | sudo tee -a /etc/fstab
+done
+
+sudo blkid -s UUID -o value /dev/sdb1 | while read line
+if [  $(grep -c steam /etc/lutris) != 0 ]; then
 do
 echo -e "\nUUID=$line /lutris  btrfs  defaults  0 0" | sudo tee -a /etc/fstab
 done
