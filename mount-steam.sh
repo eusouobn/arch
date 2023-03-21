de99510e5742f5be648265db80caddd73013ff3f
eusouobn <86791806+eusouobn@users.noreply.github.com>
2023-03-21 09:27:14 -0300

    Update mount-steam.sh

--- mount-steam.sh
+++ mount-steam.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
 
-sudo mkdir /jogos-1
-sudo mkdir /jogos-2
-sudo chown -R bn:bn /jogos-1
-sudo chown -R bn:bn /jogos-2
-sudo chmod 777 /jogos-1
-sudo chmod 777 /jogos-2
+sudo mkdir /mnt/jogos-1
+sudo mkdir /mnt/jogos-2
+sudo chown -R bn:bn /mnt/jogos-1
+sudo chown -R bn:bn /mnt/jogos-2
+sudo chmod 777 /mnt/jogos-1
+sudo chmod 777 /mnt/jogos-2
 
 sudo blkid -s UUID -o value /dev/sdb1 | while read line
 do
-echo -e "\nUUID=$line /jogos-1  btrfs  defaults  0 0" | sudo tee -a /etc/fstab
+echo -e "\nUUID=$line /mnt/jogos-1  ext4  defaults  0 0" | sudo tee -a /etc/fstab
 done
 
 sudo blkid -s UUID -o value /dev/sdb2 | while read line
 do
-echo -e "\nUUID=$line /jogos-2  btrfs  defaults  0 0" | sudo tee -a /etc/fstab
+echo -e "\nUUID=$line /mnt/jogos-2  ext4  defaults  0 0" | sudo tee -a /etc/fstab
 done
+
